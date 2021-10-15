1b5dfb410fdb039a5f6c9f71f723c2d02c2c877d
mukulasija <lit2020010@iiitl.ac.in>
2021-10-15 15:48:55 +0530

    ready for asthetic changes

--- app/src/main/java/com/example/contestalert/ContestListAdapter.kt
+++ app/src/main/java/com/example/contestalert/ContestListAdapter.kt
@@ -1,11 +1,19 @@
 package com.example.contestalert
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import org.w3c.dom.Text
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ContestListAdapter(private val listener : ContestClicked) : RecyclerView.Adapter<ContestViewHolder>() {
     private val items : ArrayList<Contest> = ArrayList()
@@ -22,11 +30,14 @@ class ContestListAdapter(private val listener : ContestClicked) : RecyclerView.A
     override fun onBindViewHolder(holder: ContestViewHolder, position: Int) {
         val currentItem = items[position]
         holder.titleView.text = currentItem.title
-        holder.starTime.text = "Start Time : "+currentItem.StartTime
-        holder.endTime.text = "End Time : "+currentItem.EndTime
+        holder.starTime.text = "Start:    "+currentItem.StartTime.getDateWithServerTimeStamp()
+        holder.endTime.text = "End  :    "+currentItem.EndTime.getDateWithServerTimeStamp()
+
+
     }
 
-    override fun getItemCount(): Int {
+
+        override fun getItemCount(): Int {
      return items.size
     }
     fun updateList(updatedList : ArrayList<Contest>)
@@ -35,7 +46,23 @@ class ContestListAdapter(private val listener : ContestClicked) : RecyclerView.A
         items.addAll(updatedList)
         notifyDataSetChanged()
     }
-}
+//    String string = "2021-10-15T15:05:00.000Z";
+//    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH);
+//    LocalDate date = LocalDate.parse(string, formatter);
+//    LocalDateTime datetime = LocalDateTime.parse(string,formatter);
+//    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a");
+//    datetime.format(format)
+
+    @SuppressLint("NewApi")
+    private fun String.getDateWithServerTimeStamp(): String {
+        val string = this
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH)
+        val datetime = LocalDateTime.parse(string,formatter)
+        val format = DateTimeFormatter.ofPattern("dd-MM-yyyy | hh:mm a")
+        return datetime.format(format)
+    }
+
+    }
 class ContestViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
 {
     val titleView : TextView = itemView.findViewById(R.id.name)
