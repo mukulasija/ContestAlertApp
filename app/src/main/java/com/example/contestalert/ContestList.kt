c28ccb6e836adc91130ba48303f8cbe4a4e461d5
mksiddiq <mksiddiq99100@gmail.com>
2022-10-28 12:27:19 +0530

    Upgraded gradle ro the latest version and added a functionality to check the internet connection as the app is fetching data from the internet.

--- app/src/main/java/com/example/contestalert/ContestList.kt
+++ app/src/main/java/com/example/contestalert/ContestList.kt
@@ -1,12 +1,17 @@
 package com.example.contestalert
 
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.provider.CalendarContract
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
+
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -37,28 +42,45 @@ class ContestList(public val site: String, public val siteName: String) :
 
     private fun fetchData() {
         val url = "https://kontests.net/api/v1/" + site
-        val jsonObjectRequest = JsonArrayRequest(
-            Request.Method.GET, url, null,
-            { response ->
-                val ContestJsonArray = response
-                val ContestArray = ArrayList<Contest>()
-                for (i in ContestJsonArray.length() - 1 downTo 0) {
-                    val ContestJsonObject = ContestJsonArray.getJSONObject(i)
-                    val contest = Contest(
-                        ContestJsonObject.getString("name"),
-                        ContestJsonObject.getString("start_time"),
-                        ContestJsonObject.getString("end_time")
-                    )
-                    ContestArray.add(contest)
+        if(ConnectionManager().checkConnectivity(activity as Context)){
+            val jsonObjectRequest = JsonArrayRequest(
+                Request.Method.GET, url, null,
+                { response ->
+                    val ContestJsonArray = response
+                    val ContestArray = ArrayList<Contest>()
+                    for (i in ContestJsonArray.length() - 1 downTo 0) {
+                        val ContestJsonObject = ContestJsonArray.getJSONObject(i)
+                        val contest = Contest(
+                            ContestJsonObject.getString("name"),
+                            ContestJsonObject.getString("start_time"),
+                            ContestJsonObject.getString("end_time")
+                        )
+                        ContestArray.add(contest)
+                    }
+                    mAdapter.updateList(ContestArray)
+                },
+                { error ->
+                    Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
                 }
-                mAdapter.updateList(ContestArray)
-            },
-            { error ->
-                Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+            )
+            // Access the RequestQueue through your singleton class.
+            MySingleton.getInstance(requireView().context).addToRequestQueue(jsonObjectRequest)
+        } else{
+            val dialog = AlertDialog.Builder(activity as Context)
+            dialog.setTitle("Error")
+            dialog.setMessage("Internet Connection Not Found")
+            dialog.setPositiveButton("Open Settings") { text, listener ->
+                val settingsIntent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                startActivity(settingsIntent)
+                ActivityCompat.finishAffinity(requireActivity())
             }
-        )
-// Access the RequestQueue through your singleton class.
-        MySingleton.getInstance(requireView().context).addToRequestQueue(jsonObjectRequest)
+            dialog.setNegativeButton("Exit App") { text, listener ->
+                ActivityCompat.finishAffinity(requireActivity())
+            }
+            dialog.create()
+            dialog.show()
+        }
+
     }
 
     override fun reminderClicked(item: Contest) {
